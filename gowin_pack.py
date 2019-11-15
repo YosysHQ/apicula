799d23e3a484a221e20b9b1ddc93b06bae6aab01
Pepijn de Vos <pepijndevos@gmail.com>
2019-11-15 18:41:32 +0100

    add bank and pips

--- gowin_pack.py
+++ gowin_pack.py
@@ -3,7 +3,7 @@ import re
 import json
 import bslib
 import fuse_h4x as fse
-from wirenames import wirenumbers
+from wirenames import wirenames, wirenumbers
 
 
 cmd_hdr = [
@@ -47,6 +47,8 @@ def infovaluemap(infovalue, start=2):
     return {tuple(iv[:start]):iv[start:] for iv in infovalue}
 
 def place(fuse, tilemap, bels):
+    width = len(fuse['header']['grid'][61][0])
+    height = len(fuse['header']['grid'][61])
     for typ, row, col, num, attr in bels:
         ttyp = fuse['header']['grid'][61][row-1][col-1]
         tiledata = fuse[ttyp]
@@ -61,8 +63,8 @@ def place(fuse, tilemap, bels):
                     fuses = lutmap[(num, bitnum)]
                     for f in fuses:
                         if f < 0: continue
-                        row, col = fse.fuse_lookup(fuse, ttyp, f)
-                        tile[row][col] = 1
+                        r, c = fse.fuse_lookup(fuse, ttyp, f)
+                        tile[r][c] = 1
 
             #if attr["FF_USED"]: # Maybe it *always* needs the DFF
             if True:
@@ -71,8 +73,8 @@ def place(fuse, tilemap, bels):
                 dffmap = infovaluemap(lutfuses)
                 for feat in [-7, 20, 21]:
                     f = dffmap[(feat, 0)][0]
-                    row, col = fse.fuse_lookup(fuse, ttyp, f)
-                    tile[row][col] = 1
+                    r, c = fse.fuse_lookup(fuse, ttyp, f)
+                    tile[r][c] = 1
         elif typ == "GENERIC_IOB":
             assert sum(attr.values()) <= 1, "Complex IOB unsuported"
             table = 23 + num
@@ -94,11 +96,61 @@ def place(fuse, tilemap, bels):
                 fuses = iobmap[feat]
                 for f in fuses:
                     if f < 0: break
-                    row, col = fse.fuse_lookup(fuse, ttyp, f)
-                    tile[row][col] = 1
+                    r, c = fse.fuse_lookup(fuse, ttyp, f)
+                    tile[r][c] = 1
+
+            #bank enable
+            if row == 1: # top bank
+                bank = 0
+                ttyp = 50
+                brow = 1
+                bcol = 1
+            elif row == height: # bottom bank
+                bank = 2
+                ttyp = 49
+                brow = height
+                bcol = width
+            elif col == 1: # left bank
+                bank = 3
+                ttyp = 48
+                brow = height
+                bcol = 1
+            elif col == width: # right bank
+                bank = 1
+                ttyp = 51
+                brow = 1
+                bcol = width
+            assert ttyp == fuse['header']['grid'][61][brow-1][bcol-1]
+            tiledata = fuse[ttyp]
+            tile = tilemap[(brow-1, bcol-1, ttyp)]
+            bankfuses = tiledata['longval'][37]
+            bankmap = infovaluemap(bankfuses, 16)
+            feat = (bank, 48)+(0,)*14 # bank, logic type?
+            fuses = bankmap[feat]
+            for f in fuses:
+                if f < 0: break
+                r, c = fse.fuse_lookup(fuse, ttyp, f)
+                tile[r][c] = 1
 
 
+def route(fuse, tilemap, pips):
+    for row, col, src, dest in pips:
+        ttyp = fuse['header']['grid'][61][row-1][col-1]
+        tiledata = fuse[ttyp]
+        tile = tilemap[(row-1, col-1, ttyp)]
 
+        wires = tiledata['wire'][2]
+        wiremap = infovaluemap(wires)
+        try:
+            fuses = wiremap[(src, dest)]
+        except KeyError:
+            if (-src, dest) not in wiremap: # default input?
+                print(wirenames[src], wirenames[dest], "not found in tile", row, col)
+            continue
+        for f in fuses:
+            if f < 0: break
+            r, c = fse.fuse_lookup(fuse, ttyp, f)
+            tile[r][c] = 1
 
 if __name__ == '__main__':
     with open(sys.argv[1], 'rb') as f:
@@ -109,6 +161,8 @@ if __name__ == '__main__':
     tilemap = fse.tile_bitmap(fuse, empty, empty=True)
     bels = get_bels(pnr)
     place(fuse, tilemap, bels)
+    pips = get_pips(pnr)
+    route(fuse, tilemap, pips)
     res = fse.fuse_bitmap(fuse, tilemap)
     fse.display('pack.png', res)
     bslib.write_bitstream('pack.fs', res, cmd_hdr, cmd_ftr)
