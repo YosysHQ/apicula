9142db33d8adeea70c82731b60159771525a7c75
Pepijn de Vos <pepijndevos@gmail.com>
2020-10-12 19:31:53 +0200

    fuzz quadrants

--- clock_fuzzer.py
+++ clock_fuzzer.py
@@ -39,7 +39,7 @@ def ibuf(mod, cst, loc, clk=None):
 
 with open(f"{tiled_fuzzer.gowinhome}/IDE/share/device/{tiled_fuzzer.device}/{tiled_fuzzer.device}.fse", 'rb') as f:
     fse = fuse_h4x.readFse(f)
-    
+
 with open(f"{tiled_fuzzer.device}.json") as f:
     dat = json.load(f)
 
@@ -56,30 +56,65 @@ true_pins = [p[0] for p in clock_pins if "GCLKT" in p[1]]
 
 pool = Pool()
 
-def center_muxes(side):
+def quadrants():
+    mod = codegen.Module()
+    cst = codegen.Constraints()
+    ibuf(mod, cst, true_pins[2], clk="myclk")
+    base_bs, _, _, _, _ = tiled_fuzzer.run_pnr(mod, cst, {})
+
+    width = len(db.grid[0])
+    height = len(db.grid)
+    modules = []
+    constrs = []
+    idxes = []
+    for i in range(2, width):
+        for j in [2, height-3]: # avoid bram
+            if "DFF0" not in db.grid[j-1][i-1].bels:
+                print(i, j)
+                continue
+            mod = codegen.Module()
+            cst = codegen.Constraints()
+
+            ibuf(mod, cst, true_pins[0], clk="myclk")
+            dff(mod, cst, j, i, clk="myclk")
+
+            modules.append(mod)
+            constrs.append(cst)
+            idxes.append((i, j))
+
+    for i in [2, width-2]:
+        for j in range(2, height):
+            if "DFF0" not in db.grid[j-1][i-1].bels:
+                print(i, j)
+                continue
+            mod = codegen.Module()
+            cst = codegen.Constraints()
+
+            ibuf(mod, cst, true_pins[0], clk="myclk")
+            dff(mod, cst, j, i, clk="myclk")
+
+            modules.append(mod)
+            constrs.append(cst)
+            idxes.append((i, j))
+
+    pnr_res = pool.map(lambda param: tiled_fuzzer.run_pnr(*param, {}), zip(modules, constrs))
+
+    res = {}
+    for (row, col), (mybs, *_) in zip(idxes, pnr_res):
+        sweep_tiles = fuse_h4x.tile_bitmap(fse, mybs^base_bs)
+        t8x = [(r, c) for (r, c, typ), t in sweep_tiles.items() if typ >= 80 and typ < 90]
+        rows, cols = res.setdefault(t8x[0], (set(), set()))
+        rows.add(row)
+        cols.add(col)
+
+    return res
+
+def center_muxes(ct, rows, cols):
     "Find which mux drives which spine, and maps their inputs to clock pins"
-    crow = dat['center'][0]
-    ccol = dat['center'][1]
-    if side == "L": # GW1N-1 has 2 sides
-        ct = crow-1, ccol-1
-        dff_locs = [(2, 2+i) for i in range(len(true_pins))]
-    elif side == "R":
-        dff_locs = [(2, ccol+1+i) for i in range(8)]
-        ct = crow-1, ccol
-    elif side == "TL": # GW1N-9 has 4 quadrants
-        ct = crow-1, ccol-2
-        dff_locs = [(2, 2+i) for i in range(len(true_pins))]
-    elif side == "TR":
-        ct = crow-1, ccol+1
-        dff_locs = [(2, ccol+1+i) for i in range(len(true_pins))]
-    elif side == "BL":
-        ct = crow-1, ccol-1
-        dff_locs = [(crow+1, 2+i) for i in range(len(true_pins))]
-    elif side == "BR":
-        ct = crow-1, ccol
-        dff_locs = [(crow+1, ccol+1+i) for i in range(len(true_pins))]
-    else:
-        raise ValueError("Wrong side")
+
+    fr = min(rows)
+    dff_locs = [(c, fr) for c in cols][:len(true_pins)]
+
     mod = codegen.Module()
     cst = codegen.Constraints()
 
@@ -111,7 +146,7 @@ def center_muxes(side):
         new = base ^ bs_sweep
         base = bs_sweep
         tiles = chipdb.tile_bitmap(db, new)
-        
+
         try:
             db_tile = db.grid[ct[0]][ct[1]]
             _, _, clk_pips = gowin_unpack.parse_tile_(db_tile, tiles[ct], default=False)
@@ -121,7 +156,7 @@ def center_muxes(side):
             # it seems this uses a dynamically configurable mux routed to VCC/VSS
             continue
         print(i, pin, src, dest)
-        gb_destinations[f"SPINE{side}{i}"] = dest
+        gb_destinations[f"SPINE_{ct[1]}_{i}"] = dest
         gb_sources[src] = pin
 
     return gb_sources, gb_destinations
@@ -172,7 +207,7 @@ def taps():
     complete_taps = set()
     for idx, (sweep_bs, *_) in enumerate(pnr_res):
         sweep_tiles = chipdb.tile_bitmap(db, sweep_bs^bs_base)
-        
+
         dffs = set()
         tap = None
         gclk = idx//(width-2)
@@ -203,27 +238,22 @@ def taps():
 
     return clks
 
-
-
 if __name__ == "__main__":
-    if tiled_fuzzer.device == "GW1N-1":
-        lsrcs, ldests = center_muxes('L')
-        true_pins.reverse()  # work around unfuzzed mux
-        rsrcs, rdests = center_muxes('R')
-        assert lsrcs == rsrcs
-        srcs = rsrcs
-        dsts = {**rdests, **ldests}
-    elif tiled_fuzzer.device == "GW1N-9":
-        tlsrcs, tldests = center_muxes('TL')
-        trsrcs, trdests = center_muxes('TR')
-        true_pins.reverse()  # work around unfuzzed mux
-        blsrcs, bldests = center_muxes('BL')
-        brsrcs, brdests = center_muxes('BR')
-        srcs = {**trsrcs, **tlsrcs, **brsrcs, **blsrcs}
-        dsts = {**trdests, **tldests, **brdests, **bldests}
+    quads = quadrants()
+
+    srcs = {}
+    dests = {}
+    for ct, (rows, cols) in quads.items():
+        # I reverse the pins here because
+        # the 8th mux is not fuzzed presently
+        true_pins.reverse()
+        qsrcs, qdests = center_muxes(ct, rows, cols)
+        srcs.update(qsrcs)
+        dests.update(qdests)
 
     clks = taps()
 
+    print(quads)
     print(srcs)
-    print(dsts)
+    print(dests)
     print(clks)
