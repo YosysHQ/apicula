e0a86c35ea48b4c23949b4df201773e8c3957a0e
YRabbit <rabbit@yrabbit.cyou>
2022-02-24 08:20:58 +1000

    Extend the TLVDS example to the Tangnano9k board
    Signed-off-by: YRabbit <rabbit@yrabbit.cyou>

--- examples/tlvds.v
+++ examples/tlvds.v
@@ -1,8 +1,10 @@
-/* Using an oscilloscope you can see on pins 27, 28 the signal with changing polarity.
+/* Using an oscilloscope you can see on pins tlvds_p and tlvds_n the signal with changing polarity.
+* You can also use a 100 Ohm resistor and two LEDs connected in opposite directions between these pins.
 */
 module top (
     input clk,
-    output [1:0]led
+    output tlvds_p,
+    output tlvds_n,
 );
 
 reg [25:0] ctr_q;
@@ -15,11 +17,11 @@ always @(posedge clk)
 
 // Combinational code (boolean logic)
 assign ctr_d = ctr_q + 1'b1;
-assign i_tick = ctr_q[25:25];
+assign i_tick = |ctr_q[25:24];
 
 TLVDS_OBUF diff_buf(
-        .O(led[1]),
-        .OB(led[0]),
+        .O(tlvds_p),
+        .OB(tlvds_n),
         .I(i_tick)
     );
 
