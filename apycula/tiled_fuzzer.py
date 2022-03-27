d9d7249711a6a711b92fcf753872b67aee700810
Tim Pambor <tp@pambor.de>
2022-03-27 01:22:18 +0100

    Add oscillator to chipdb

--- apycula/tiled_fuzzer.py
+++ apycula/tiled_fuzzer.py
@@ -917,7 +917,7 @@ if __name__ == "__main__":
     with open(f"{gowinhome}/IDE/share/device/{device}/{device}.tm", 'rb') as f:
         tm = tm_h4x.read_tm(f, device)
 
-    db = chipdb.from_fse(fse)
+    db = chipdb.from_fse(device, fse)
     chipdb.set_banks(device, db)
     db.timing = tm
     db.packages, db.pinout, db.pin_bank = chipdb.json_pinout(device)
