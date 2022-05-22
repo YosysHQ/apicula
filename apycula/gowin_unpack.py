beb5d47be167a00cf3bac01924b54d9a0cf42d35
YRabbit <rabbit@yrabbit.cyou>
2022-05-22 16:36:08 +1000

    gowin_unpack: Change the constant representation (#104)
    * gowin_unpack: Change the constant representation
    
    Gowin tools do not understand dimensionless constant assignments and,
    oddly enough, binary constants in the INIT parameter. So we specify the
    dimensionality in the first case and use the hexadecimal representation
    in the second.
    
    Signed-off-by: YRabbit <rabbit@yrabbit.cyou>
    
    * unpack: Port blanking plug Q1 ODDR
    
    Also Gowin tools do not digest when port Q1 for ODDR primitive is not
    specified, but I do not want to fix it now because the right way is to
    fully implement ODDR with TBUF.
    
    Signed-off-by: YRabbit <rabbit@yrabbit.cyou>
    
    * unpack: Do not generate an unnamed wire
    
    Signed-off-by: YRabbit <rabbit@yrabbit.cyou>

--- apycula/gowin_unpack.py
+++ apycula/gowin_unpack.py
@@ -285,7 +285,7 @@ def tile2verilog(dbrow, dbcol, bels, pips, clock_pips, mod, cst, db):
             else:
                 name = f"R{row}C{col}_LUT4_{idx}"
                 lut = codegen.Primitive("LUT4", name)
-                lut.params["INIT"] = f"16'b{val:016b}"
+                lut.params["INIT"] = f"16'h{val:04x}"
                 lut.portmap['F'] = f"R{row}C{col}_F{idx}"
                 lut.portmap['I0'] = f"R{row}C{col}_A{idx}"
                 lut.portmap['I1'] = f"R{row}C{col}_B{idx}"
@@ -380,6 +380,8 @@ def tile2verilog(dbrow, dbcol, bels, pips, clock_pips, mod, cst, db):
             oddr.portmap['Q0'] = f"R{row}C{col}_{portmap['D0']}_IOL"
             #oddr.portmap['Q1'] = f"R{row}C{col}_{portmap['Q1']}_IOL"
             mod.wires.update(oddr.portmap.values())
+            # XXX implement ODDR with TBUF
+            oddr.portmap['Q1'] = ""
             mod.primitives[name] = oddr
         elif typ == "IOB":
             try:
@@ -430,8 +432,8 @@ def tile2verilog(dbrow, dbcol, bels, pips, clock_pips, mod, cst, db):
     # vcc = codegen.Primitive("VCC", "myvcc")
     # vcc.portmap["V"] = "VCC"
     # mod.primitives["myvcc"] = vcc
-    mod.assigns.append(("VCC", "1"))
-    mod.assigns.append(("VSS", "0"))
+    mod.assigns.append(("VCC", "1'b1"))
+    mod.assigns.append(("VSS", "1'b0"))
 
 def default_device_config():
     return {
