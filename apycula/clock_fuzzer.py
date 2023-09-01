6ed6922935bf10014a83fe4c72dab4ad416eaa07
YRabbit <rabbit@yrabbit.cyou>
2023-09-01 20:56:36 +1000

    Enable all legacy checks
    Signed-off-by: YRabbit <rabbit@yrabbit.cyou>

--- apycula/clock_fuzzer.py
+++ apycula/clock_fuzzer.py
@@ -357,7 +357,7 @@ def make_lw_aliases(fse, dat, db, quads, clks):
                     db.aliases.update({(row, col, f'LB{lw + 4}1') : (row, tap_col, f'LBO1')})
 
 if __name__ == "__main__":
-    if False:
+    if True:
         quads = quadrants()
 
         srcs = {}
