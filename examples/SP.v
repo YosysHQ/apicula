4508b557ad1b08777a1a8661ade13d154d74231b
YRabbit <rabbit@yrabbit.cyou>
2025-09-29 18:26:43 +1000

    PLL in a separate module.
    The GW5A series has a different PLL, so we will adapt our examples to
    future support for the GW5A series.
    
    Signed-off-by: YRabbit <rabbit@yrabbit.cyou>

--- examples/SP.v
+++ examples/SP.v
@@ -21,43 +21,12 @@ module top
 	wire gnd;
 	assign gnd = 1'b0;
 
-	rPLL pll(
-	    .CLKOUT(pixel_clk),  // 9MHz
-		.CLKOUTD(write_clk),
-		.CLKIN(clk),
-		.CLKFB(gnd),
-		.RESET(!rst),
-		.RESET_P(!rst),
-		.FBDSEL({gnd, gnd, gnd, gnd, gnd, gnd}),
-		.IDSEL({gnd, gnd, gnd, gnd, gnd, gnd}),
-		.ODSEL({gnd, gnd, gnd, gnd, gnd, gnd}),
-		.DUTYDA({gnd, gnd, gnd, gnd}),
-		.PSDA({gnd, gnd, gnd, gnd}),
-		.FDLY({gnd, gnd, gnd, gnd})
-	);
-	defparam pll.DEVICE = `PLL_DEVICE;
-	defparam pll.FCLKIN = `PLL_FCLKIN;
-	defparam pll.FBDIV_SEL = `PLL_FBDIV_SEL_LCD;
-	defparam pll.IDIV_SEL =  `PLL_IDIV_SEL_LCD;
-	defparam pll.ODIV_SEL = `PLL_ODIV_SEL;
-	defparam pll.CLKFB_SEL="internal";
-	defparam pll.CLKOUTD3_SRC="CLKOUT";
-	defparam pll.CLKOUTD_BYPASS="false";
-	defparam pll.CLKOUTD_SRC="CLKOUT";
-	defparam pll.CLKOUTP_BYPASS="false";
-	defparam pll.CLKOUTP_DLY_STEP=0;
-	defparam pll.CLKOUTP_FT_DIR=1'b1;
-	defparam pll.CLKOUT_BYPASS="false";
-	defparam pll.CLKOUT_DLY_STEP=0;
-	defparam pll.CLKOUT_FT_DIR=1'b1;
-	defparam pll.DUTYDA_SEL="1000";
-	defparam pll.DYN_DA_EN="false";
-	defparam pll.DYN_FBDIV_SEL="false";
-	defparam pll.DYN_IDIV_SEL="false";
-	defparam pll.DYN_ODIV_SEL="false";
-	defparam pll.DYN_SDIV_SEL=128;
-	defparam pll.PSDA_SEL="0000";
-
+	clock_pll clok_pll(
+		.clk(clk),
+		.rst(rst),
+		.write_clk(write_clk),
+		.pixel_clk(pixel_clk));
+	
 	assign		LCD_CLK		=	pixel_clk;
 
     reg         [15:0]  pixel_count;
